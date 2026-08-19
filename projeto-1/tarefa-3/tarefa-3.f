25db5bf18f14579851cb5c4cd648c65db4bcf4a1
jhapreis <jhapreis@gmail.com>
2026-08-18 21:06:58 -0300

    fix: correcao da tarefa 2

--- projeto-1/tarefa-3/tarefa-3.f
+++ projeto-1/tarefa-3/tarefa-3.f
@@ -1,19 +1,18 @@
+	implicit none
+
 	! Definição dos números n (o tamanho do arquivo de input) e m (o subconjunto de menores valores)
 	integer n, m
 	
 	! Definição de variáveis auxiliares para loop dentro do arquivo
 	integer i, j
 	
-	! Definição das unidades dos arquivos
-	integer unit_file_in, unit_file_out
-	
 	! Valores da linha do arquivo, do atual candidato e do último salvo até então 
-	double precision row, val_min, last_min, inf
+	double precision vl_row, vl_min, vl_min_last
 	
-	inf = 1.0d12
+	! Valor máximo, para comparação. Uma constante, interpretada como infinito
+	double precision inf
 	
-	val_min = inf
-	last_min = inf
+	inf = 1.0d300
 	
 	print *, "Insira o tamanho do arquivo original: "
 	read (*,*) n
@@ -21,36 +20,56 @@
 	print *, "Insira o tamanho do subconjunto de menores valores: "
 	read (*,*) m
 	
+	if( (n.lt.0).or.(m.lt.0) ) then
+		print *, 'ERROR: (m,n) devem ser não-negativos: ', m, n
+		stop
+	else if(n.lt.m) then
+		print *, 'ERROR: m não pode ser maior que n', m, n
+		stop
+	end if
+
 	! Abrir arquivo de saída e salvar o valor de m
-	open(unit=99, file="out.dat")
+	open(unit=99, file="tarefa-3-saida-1.dat")
 	
 	write(99, *) m
 	
-	do i=0,m
+	! Para iniciar o loop, consideramos que o menor já valor encontrado é -inf
+	vl_min_last = -inf
 	
+	do i=1,m
+	
+		! Start/reset do vl_min do loop
+		vl_min = inf
+
 		! Ler o arquivo de dados de input
-		open(unit=1, file="dados.dat")
+		open(unit=1, file="tarefa-3-entrada-1.in")
 
-		do j=0,n
+		do j=1,n
 		
-			read(1, *) row
+			read(1, *) vl_row
 
-			if(j.eq.0) then
-				val_min = row
+			if(j.eq.1) then
+				vl_min = vl_row
 			else
-				if(row.lt.val_min).and.(row.lt.last_min) then
-					val_min = row	
+				if( (vl_row.lt.vl_min).and.(vl_row.gt.vl_min_last) ) then
+					vl_min = vl_row	
 				end if
 			
 			end if
 			
+			! print *, 'j=', j, 'vl_min=', vl_min, 'vl_min_last=', vl_min_last
+			
 		end do
 		
-		last_min = val_min
+		! print *, 'i=', i, 'vl_min=', vl_min
 		
-		print *, i, row, val_min, last_min
+		write(99, *) vl_min
 		
+		! Fecha o arquivo, para ler novamente do começo no próximo laço
 		close(1)
+		
+		! Define o menor valor como sendo o último mínimo encontrado
+		vl_min_last = vl_min
 		
 	end do
 		
